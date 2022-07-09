1023a50eba8d51800e24644cafd78c132bf56dba
Antonio Sarno <>
2022-07-09 05:01:26 +0200

    Extensions for keyboard

--- app/src/main/java/com/pizza11x/androidtoolkit/utils/GeneralExtensions.kt
+++ app/src/main/java/com/pizza11x/androidtoolkit/utils/GeneralExtensions.kt
@@ -1,7 +1,11 @@
 package com.pizza11x.androidtoolkit.utils
 
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
@@ -11,4 +15,10 @@ fun <T : ViewDataBinding> bindLayout(
     viewGroup: ViewGroup
 ): T {
     return DataBindingUtil.inflate(inflater, layout, viewGroup, false)
+}
+
+fun Context.closeSoftKeyboard(view: View) {
+    val inputMethodManager =
+        getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 }
--- app/src/main/java/com/pizza11x/androidtoolkit/utils/PresentetionExtensions.kt
+++ app/src/main/java/com/pizza11x/androidtoolkit/utils/PresentetionExtensions.kt
@@ -1,8 +1,12 @@
 package com.pizza11x.androidtoolkit.utils
 
 import android.app.Activity
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
 
 /*
     Binding inflate Layout
@@ -10,4 +14,18 @@ import androidx.databinding.ViewDataBinding
 fun <T : ViewDataBinding> Activity.bindLayout(layout: Int) : T {
     return DataBindingUtil.setContentView(this, layout)
 
+}
+
+fun Fragment.openSoftKeyboard(view: View){
+    if(view.requestFocus()){
+        activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
+    }
+}
+
+fun Fragment.closeSoftKeyboard(){
+    view?.let { activity?.closeSoftKeyboard(it) }
+}
+
+fun AppCompatActivity.closeSoftKeyboard(){
+    currentFocus?.let { closeSoftKeyboard(it) }
 }
