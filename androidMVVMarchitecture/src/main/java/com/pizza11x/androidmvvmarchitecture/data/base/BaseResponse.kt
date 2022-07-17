86d27fa70b2905f10e5d916b075c4ae15e0b5ad2
Antonio Sarno <>
2022-07-17 17:24:40 +0200

    Fix data base classes

--- androidMVVMarchitecture/src/main/java/com/pizza11x/androidmvvmarchitecture/data/base/BaseResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.pizza11x.androidmvvmarchitecture.data.base
-
-class BaseResponse (
-    var id: String? = null,
-    var code: String? = null,
-    var message: String? = null
-)
---
+++ androidMVVMarchitecture/src/main/java/com/pizza11x/androidmvvmarchitecture/data/base/OBaseResponse.kt
@@ -0,0 +1,7 @@
+package com.pizza11x.androidmvvmarchitecture.data.base
+
+open class OBaseResponse (
+    open var id: String? = null,
+    open var code: String? = null,
+    open var message: String? = null
+)
