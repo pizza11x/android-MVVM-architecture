48444f211ee744f9af7ec78941159576890aae38
Antonio Sarno <>
2022-07-17 02:03:17 +0200

    Fix ANavDialogFragment name

--- androidMVVMarchitecture/src/main/java/com/pizza11x/androidmvvmarchitecture/presentation/navigation/base/ANavDialogFragment.kt
+++ androidMVVMarchitecture/src/main/java/com/pizza11x/androidmvvmarchitecture/presentation/navigation/base/ANavDialogFragment.kt
@@ -5,7 +5,7 @@ import androidx.databinding.ViewDataBinding
 import com.pizza11x.androidmvvmarchitecture.presentation.ui.base.ABaseDialogFragment
 import com.pizza11x.androidmvvmarchitecture.presentation.viewmodels.base.INavViewModel
 
-abstract class ABaseNavDialogFragment<DialogBinding : ViewDataBinding>(@LayoutRes layout: Int) :
+abstract class ANavDialogFragment<DialogBinding : ViewDataBinding>(@LayoutRes layout: Int) :
     ABaseDialogFragment<DialogBinding>(layout) {
     /* VARIABLES */
     protected abstract val viewModel: INavViewModel
