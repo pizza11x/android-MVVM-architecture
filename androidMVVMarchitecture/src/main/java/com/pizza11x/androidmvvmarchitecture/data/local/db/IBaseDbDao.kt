c1a52dd391126e9589b39be8fc4378cb1bfaf402
Antonio Sarno <>
2022-07-23 19:21:54 +0200

    Fix Base DAO interface

--- androidMVVMarchitecture/src/main/java/com/pizza11x/androidmvvmarchitecture/data/local/db/IBaseDbDao.kt
+++ androidMVVMarchitecture/src/main/java/com/pizza11x/androidmvvmarchitecture/data/local/db/IBaseDbDao.kt
@@ -1,6 +1,7 @@
 package com.pizza11x.androidmvvmarchitecture.data.local.db
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.*
 import androidx.room.Update
@@ -12,8 +13,21 @@ interface IBaseDbDao {
     @Insert(onConflict = REPLACE)
     suspend fun insert(entity: OBaseDbEntity): Long
 
+    @Insert(onConflict = REPLACE)
+    suspend fun insert(entities: List<OBaseDbEntity>)
+
     /* UPDATE FUN */
-    @Update()
-    suspend fun update(entity: OBaseDbEntity): Int
+    @Update
+    suspend fun update(entity: OBaseDbEntity)
+
+    @Update
+    suspend fun update(entities: List<OBaseDbEntity>)
+
+    /* DELETE FUN */
+    @Delete
+    fun delete(entity: OBaseDbEntity)
+
+    @Delete
+    fun delete(entities: List<OBaseDbEntity>)
 
 }
